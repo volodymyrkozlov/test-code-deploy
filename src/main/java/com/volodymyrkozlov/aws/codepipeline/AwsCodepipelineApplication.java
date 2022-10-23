4f4067c2e75189cc803ec434112473f58e09a83b
Volodymyr Kozlov <volodymyr.kozlow@gmail.com>
2022-10-23 22:49:33 +0300

    intial commit

---
+++ src/main/java/com/volodymyrkozlov/aws/codepipeline/AwsCodepipelineApplication.java
@@ -0,0 +1,13 @@
+package com.volodymyrkozlov.aws.codepipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCodepipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCodepipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/volodymyrkozlov/aws/codepipeline/AwsCodepipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volodymyrkozlov.aws.codepipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCodepipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
